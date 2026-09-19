ad1ad211a54b084b2042e10a3f3595b05a516985
dtrabuc <75096686+dtrabuc@users.noreply.github.com>
2026-09-19 02:41:35 +0200

    Allow employees to manage menus

--- App/Controller/AdminController.php
+++ App/Controller/AdminController.php
@@ -396,7 +396,7 @@ class AdminController extends BaseController
         // Apply auth middleware
         (new \App\Middleware\Auth())();
         // Apply admin middleware
-        (new \App\Middleware\Admin())();
+        (new \App\Middleware\Staff())();
 
         if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
             http_response_code(405);
@@ -413,7 +413,7 @@ class AdminController extends BaseController
         // Apply auth middleware
         (new \App\Middleware\Auth())();
         // Apply admin middleware
-        (new \App\Middleware\Admin())();
+        (new \App\Middleware\Staff())();
 
         if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
             http_response_code(405);
@@ -477,7 +477,7 @@ class AdminController extends BaseController
         // Apply auth middleware
         (new \App\Middleware\Auth())();
         // Apply admin middleware
-        (new \App\Middleware\Admin())();
+        (new \App\Middleware\Staff())();
 
         if ($_SERVER['REQUEST_METHOD'] !== 'PUT' && $_SERVER['REQUEST_METHOD'] !== 'POST') {
             // Handle form submission via POST with _method=PUT
@@ -550,7 +550,7 @@ class AdminController extends BaseController
         // Apply auth middleware
         (new \App\Middleware\Auth())();
         // Apply admin middleware
-        (new \App\Middleware\Admin())();
+        (new \App\Middleware\Staff())();
 
         if ($_SERVER['REQUEST_METHOD'] !== 'DELETE' && $_SERVER['REQUEST_METHOD'] !== 'POST') {
             http_response_code(405);
